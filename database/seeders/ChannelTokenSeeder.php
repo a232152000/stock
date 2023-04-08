577d87f8a7ae222eaa555e99a01bd0d38eae4aa8
a232152000 <a232152000@gmail.com>
2023-04-08 11:12:45 +0800

    add seeder

---
+++ database/seeders/ChannelTokenSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ChannelTokenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('channel_tokens')->insert([
+            'token' => 'U55cfec471cde3a870a91c5c372258fd1',
+            'channel' => 'line',
+            'created_at' => '2023-03-25 14:15:00',
+            'updated_at' => '2023-03-25 14:15:00'
+        ]);
+    }
+}
