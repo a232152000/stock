374e0522d885d387adbd6a4f143ba8207e50b332
a232152000 <a232152000@gmail.com>
2023-05-06 13:39:14 +0800

    [feat]channel tokens新增一個user_id欄位

--- database/seeders/ChannelTokenSeeder.php
+++ database/seeders/ChannelTokenSeeder.php
@@ -14,6 +14,7 @@ class ChannelTokenSeeder extends Seeder
     public function run(): void
     {
         DB::table('channel_tokens')->insert([
+            'user_id' => 1,
             'token' => 'U55cfec471cde3a870a91c5c372258fd1',
             'channel' => 'line',
             'created_at' => '2023-03-25 14:15:00',
